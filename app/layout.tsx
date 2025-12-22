d7e094c395a3b1e08d49d5b9a10e9ea5d79d5e65
Stevan Borus <steva.borus@gmail.com>
2025-12-22 16:18:02 +0100

    fix favicons and og

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,11 +56,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon-16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-180.png', sizes: '180x180', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-180x180.png', sizes: '180x180', type: 'image/png' },
     ],
-    apple: [{ url: '/favicon-180.png', sizes: '180x180', type: 'image/png' }],
+    apple: [
+      { url: '/favicon-180x180.png', sizes: '180x180', type: 'image/png' },
+    ],
   },
   openGraph: {
     type: 'website',
